d5579309784088974532bb04e342aa8c67ed9df6
hirossan4049 <haruto405329@gmail.com>
2021-03-04 23:27:40 +0900

    update

---
+++ install.sh
@@ -0,0 +1,22 @@
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
+if [`uname` = "Darwin"]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew bundle
+
+
+elif [ `uname` = "Linux" ]; then
+    sudo apt install vim tmux git zsh
+fi
+
+mkdir -p ~/fun
+
+
+echo install onedark
+sh onedark_install.sh
+
+echo install oh my zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+
--- onedark_install.sh
+++ onedark_install.sh
@@ -1,7 +1,7 @@
 echo Install Onedark theme Start!
 git clone https://github.com/joshdick/onedark.vim
 
-mkdir -p .vim/colors
+mkdir -p ~/.vim/colors
 mv onedark.vim/colors/* ~/.vim/colors/
 mkdir -p ~/.vim/autoload
 mv onedark.vim/autoload/onedark.vim ~/.vim/autoload
